70ff54ff9f934273809eadd0d76c046f8730b07b
Richie Cedillo <rcedilloal@gmail.com>
2020-07-11 16:08:03 -0500

    [add]bulma config

--- sesion04/getting-started-winth-webpack/src/index.js
+++ sesion04/getting-started-winth-webpack/src/index.js
@@ -1,3 +1,6 @@
-require ('./sass/styles.scss')
+//require ('./sass/styles.scss')
+//require('@fortawesome/fontawesome-free/js/all')
+import './sass/styles.scss'
+import '@fortawesome/fontawesome-free/js/all'
 const sayHello = 'Getting started with webpack'
 console.log(sayHello)
---
+++ sesion04/getting-started-with-bulma/src/index.js
@@ -0,0 +1,6 @@
+import './sass/mybulma.scss'
+import './sass/styles.scss'
+import '@fortawesome/fontawesome-free/js/all'
+
+const sayHello = 'Getting started with bulma'
+console.log(sayHello)

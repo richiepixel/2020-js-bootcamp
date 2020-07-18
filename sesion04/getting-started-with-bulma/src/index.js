184dea7541675e2921c06bce3d69199a87748a93
Richie Cedillo <rcedilloal@gmail.com>
2020-07-18 10:11:34 -0500

    [chg] Use custom config

--- sesion04/getting-started-with-bulma/src/index.js
+++ sesion04/getting-started-with-bulma/src/index.js
@@ -1,4 +1,5 @@
-import './sass/mybulma.scss'
+// import 'bulma/bulma.sass' //do not do this, you need to modify bulma's variables 
+import './sass/mybulma.scss' // recommended way
 import './sass/styles.scss'
 import '@fortawesome/fontawesome-free/js/all'
 

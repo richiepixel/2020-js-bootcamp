4e4aab4c3137cae3d69c4f6d21b6756a63189f1f
Richie Cedillo <rcedilloal@gmail.com>
2020-08-01 16:14:48 -0500

    [chg] Dashboard con bootstrap y chart js

---
+++ sesion07/bootstrap-dashboard/src/index.js
@@ -0,0 +1,8 @@
+import './sass/styles.scss'
+import '@fortawesome/fontawesome-free/js/all'
+import 'chart.js'
+import './js/bootstrap-bar-chart'
+import './js/bootstrap-line-chart'
+
+const sayHello = 'bootstrap dashboard'
+console.log(sayHello)

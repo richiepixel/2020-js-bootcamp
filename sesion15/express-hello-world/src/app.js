e29bc24adddaa66adf67b6918ac6343b6b14914d
Richie Cedillo <rcedilloal@gmail.com>
2020-09-26 11:55:24 -0500

    [chg] app.js --> http-helllo-world

--- sesion15/express-hello-world/src/app.js
+++ sesion15/express-hello-world/src/app.js
@@ -1,9 +1,11 @@
-const http = require('http');
+//const http = require('http');
+const express = require('express');
+const app = express()
 
-const hostname = '127.0.0.1';
+//const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+/* const server = http.createServer((req, res) => {
   switch (req.url) {
     case '/':
       res.statusCode = 200;
@@ -20,9 +22,41 @@ const server = http.createServer((req, res) => {
       }));
       break
   }
-});
+}); */
+
+app.get('/', (req, res) => {
+  res.end(`Hello World with express! [${req.method}] [${req.url}]`)
+})
+
+.post('/',(req, res) => {
+  res.end(`Hello World with express! [${req.method}] [${req.url}]`)
+})
+
+.put('/', (req, res) => {
+  res.end(`Hello World with express! [${req.method}] [${req.url}]`)
+})
+
+.delete('/', (req, res) => {
+  res.end(`Hello World with express! [${req.method}] [${req.url}]`)
+})
+
+app.get('/login', (req, res) => {
+  res.end(JSON.stringify({
+    success: true,
+    datetime: new Date().toISOString(),
+    token: 'qwerty.poiuyt.asdfgh',
+    msg: '[POST] /login'
+  }))
+})
 
 
-server.listen(port, hostname, () => {
+
+
+
+/* server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+}); */
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}/`)
+})
